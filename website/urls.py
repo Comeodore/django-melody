83a2dd780b17028f5213634c00d617f7b59a799b
Vladimir Maksymchuk <55732318+Comeodore@users.noreply.github.com>
2022-01-23 17:18:54 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from reserveflats import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reserveflats.urls')),
-    path(r'^.well-known/acme-challenge/.*$', views.acme_challenge, name='acme-challenge'),
 ]
